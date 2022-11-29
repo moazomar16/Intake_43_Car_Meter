594ef6e36b11538155d43754a2c2b9fa41d2b74b
Moaz Omar <moazomar16@gmail.com>
2022-11-29 16:50:18 +0200

    alarm samples and alarm test calss

---
+++ Alarm_Samples/alarm_test/Alarm_Test/src/Alarm_Test.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author moazo
+ */
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/**
+ *
+ * @author moazo
+ */
+
+
+public class Alarm_Test 
+{
+  
+    Clip clip;
+    AudioInputStream audioInputStream;
+    static String filePath;
+  
+    // constructor to initialize streams and clip
+    public Alarm_Test() throws UnsupportedAudioFileException, IOException, LineUnavailableException 
+    {
+        // create AudioInputStream object
+        audioInputStream = 
+                AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile()); 
+        // create clip reference
+        clip = AudioSystem.getClip();
+          
+        // open audioInputStream to the clip
+        clip.open(audioInputStream);
+          
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+  
+    public static void main(String[] args) 
+    {
+        try
+        {
+            filePath = "/E:/Moaz/Embedded Systems ITI/Java/java project/Alarm_Samples/Alarm-Fast-A1.aiff";
+            Alarm_Test audioPlayer = new Alarm_Test();
+             while (true)
+                {
+                    audioPlayer.clip.start();
+                }
+        } 
+          
+        catch (IOException | LineUnavailableException | UnsupportedAudioFileException ex) 
+        {
+            System.out.println("Error with playing sound.");
+          
+          }
+    }
+}
+
